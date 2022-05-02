3da430567b61071ad7e847e38b0932e6c4d58abf
kiran-rhv <r.kiran@dollarbirdinc.com>
2022-05-02 16:42:23 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeaturesComponent } from './Screens/features/features.component';
+import { LoginComponent } from './Screens/login/login.component';
+import { HomescreenComponent } from './Screens/homescreen/homescreen.component';
+import { UsersComponent } from './Screens/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomescreenComponent },
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: '', redirectTo: 'features', pathMatch: 'full' },
+  { path: 'features', component: FeaturesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './Components/header/header.component';
+import { UsersComponent } from './Screens/users/users.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { FeaturesComponent } from './Screens/features/features.component';
+import { LoginComponent } from './Screens/login/login.component';
+import { HomescreenComponent } from './Screens/homescreen/homescreen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersComponent,
+    FooterComponent,
+    FeaturesComponent,
+    LoginComponent,
+    HomescreenComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
