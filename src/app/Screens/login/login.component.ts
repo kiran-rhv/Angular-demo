b4826743d5b91f6e951e31a67f412e70d01e1223
kiran-rhv <r.kiran@dollarbirdinc.com>
2022-05-05 16:06:53 +0530

    Login Integration

--- src/app/Screens/login/login.component.ts
+++ src/app/Screens/login/login.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  form: any = {
+    email: null,
+    password: null,
+  };
 
-  constructor() { }
+  isloggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
 
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService
+  ) {}
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isloggedIn = true;
+      this.roles = this.tokenStorage.getUser().roles;
+    }
   }
-
+  onSubmit(): void {
+    const { email, password } = this.form;
+    this.authService.login(email, password).subscribe({
+      next: (data) => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
+        this.isLoginFailed = false;
+        this.isloggedIn = true;
+        this.roles = this.tokenStorage.getUser().roles;
+        this.reloadPage();
+      },
+      error: (err) => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      },
+    });
+  }
+  reloadPage(): void {
+    window.location.reload();
+  }
 }
